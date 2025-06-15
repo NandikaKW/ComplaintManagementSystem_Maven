3a819596af70ed276d8d35f1d651f0e723da9f1c
NandikaKW <nandikakaweesha4005@gmail.com>
2025-06-15 15:01:35 +0530

    complaintservlet updated

--- ComplaintManagementSystem_Maven/src/main/java/controller/ComplaintServlet.java
+++ ComplaintManagementSystem_Maven/src/main/java/controller/ComplaintServlet.java
@@ -9,5 +9,18 @@ import model.User;
 import java.io.IOException;
 @WebServlet("/ComplaintServlet")
 public class ComplaintServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        User user = (User) session.getAttribute("user");
+        if (user != null) {
+            String title = req.getParameter("title");
+            String description = req.getParameter("description");
+            Complaint c = new Complaint(title, description, user.getId());
+            ComplaintDAO.saveComplaint(c);
+            res.sendRedirect("pages/complaints.jsp");
+        } else {
+            res.sendRedirect("pages/login.jsp");
+        }
+    }
 
 }
