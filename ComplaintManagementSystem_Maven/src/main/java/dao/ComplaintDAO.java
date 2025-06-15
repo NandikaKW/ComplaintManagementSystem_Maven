28f525e352b0177f31c1325894b1bff5ec1a0533
NandikaKW <nandikakaweesha4005@gmail.com>
2025-06-15 15:07:22 +0530

    complaintdao commit

--- ComplaintManagementSystem_Maven/src/main/java/dao/ComplaintDAO.java
+++ ComplaintManagementSystem_Maven/src/main/java/dao/ComplaintDAO.java
@@ -20,5 +20,29 @@ public class ComplaintDAO {
         }
     }
 
+    public static List<Complaint> getComplaintsByUserId(int userId) {
+        List<Complaint> list = new ArrayList<>();
+        try (Connection conn = DBConnection.getDataSource().getConnection();
+             PreparedStatement ps = conn.prepareStatement(
+                     "SELECT * FROM complaints WHERE user_id = ?"
+             )) {
+            ps.setInt(1, userId);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Complaint c = new Complaint(
+                        rs.getInt("id"),
+                        rs.getString("title"),
+                        rs.getString("description"),
+                        rs.getInt("user_id"),
+                        rs.getString("status")
+                );
+                list.add(c);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
 
 }
