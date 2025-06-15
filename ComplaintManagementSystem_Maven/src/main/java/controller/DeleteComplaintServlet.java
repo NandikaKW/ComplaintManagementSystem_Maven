3cec5cb2e8e2fe35f861e1a6fc9715e55651210f
NandikaKW <nandikakaweesha4005@gmail.com>
2025-06-15 15:02:14 +0530

    deleteservlet updated

--- ComplaintManagementSystem_Maven/src/main/java/controller/DeleteComplaintServlet.java
+++ ComplaintManagementSystem_Maven/src/main/java/controller/DeleteComplaintServlet.java
@@ -9,5 +9,16 @@ import model.User;
 
 @WebServlet("/DeleteComplaintServlet")
 public class DeleteComplaintServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        User user = (User) session.getAttribute("user");
+        if (user != null) {
+            int complaintId = Integer.parseInt(req.getParameter("id"));
+            ComplaintDAO.deleteComplaint(complaintId);
+            res.sendRedirect("pages/complaints.jsp");
+        } else {
+            res.sendRedirect("pages/login.jsp");
+        }
+    }
 
 }
