03034866f1cd74f286ee7386e6689b1e7bc69b12
NandikaKW <nandikakaweesha4005@gmail.com>
2025-06-15 15:03:48 +0530

    loginservlet updated

--- ComplaintManagementSystem_Maven/src/main/java/controller/LoginServlet.java
+++ ComplaintManagementSystem_Maven/src/main/java/controller/LoginServlet.java
@@ -7,5 +7,18 @@ import java.io.IOException;
 
 
 public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        System.out.println("hello");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        User user = UserDAO.validateUser(username, password);
+        if (user != null) {
+            HttpSession session = req.getSession();
+            session.setAttribute("user", user);
+            res.sendRedirect("pages/dashboard.jsp");
+        } else {
+            res.sendRedirect("pages/login.jsp");
+        }
+    }
 
 }
