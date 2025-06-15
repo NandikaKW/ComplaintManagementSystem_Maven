b150e0581e4821fa00373470a50d03ad9eb61660
NandikaKW <nandikakaweesha4005@gmail.com>
2025-06-15 13:40:59 +0530

    admincomplaint servlet

--- ComplaintManagementSystem_Maven/src/main/java/controller/AdminComplaintServlet.java
+++ ComplaintManagementSystem_Maven/src/main/java/controller/AdminComplaintServlet.java
@@ -9,6 +9,27 @@ import model.User;
 
 @WebServlet("/AdminComplaintServlet")
 public class AdminComplaintServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        User user = (User) session.getAttribute("user");
+
+        if (user != null && "Admin".equalsIgnoreCase(user.getRole())) {
+            String action = req.getParameter("action");
+            int complaintId = Integer.parseInt(req.getParameter("id"));
+
+            if ("update".equals(action)) {
+                String status = req.getParameter("status");
+                // Remove the remarks parameter
+                ComplaintDAO.updateComplaintStatus(complaintId, status);
+            } else if ("delete".equals(action)) {
+                ComplaintDAO.deleteComplaint(complaintId);
+            }
+
+            res.sendRedirect("pages/admin_complaints.jsp");
+        } else {
+            res.sendRedirect("pages/login.jsp");
+        }
+    }
 
 
 }
