ee42174f85a085502555f1c514ec38f5a81a7ed1
NandikaKW <nandikakaweesha4005@gmail.com>
2025-06-15 15:07:01 +0530

    complaintdao commit

--- ComplaintManagementSystem_Maven/src/main/java/dao/ComplaintDAO.java
+++ ComplaintManagementSystem_Maven/src/main/java/dao/ComplaintDAO.java
@@ -6,6 +6,19 @@ import java.sql.*;
 import java.util.*;
 
 public class ComplaintDAO {
+    public static void saveComplaint(Complaint c) {
+        try (Connection conn = DBConnection.getDataSource().getConnection();
+             PreparedStatement ps = conn.prepareStatement(
+                     "INSERT INTO complaints (title, description, user_id, status) VALUES (?, ?, ?, 'Pending')"
+             )) {
+            ps.setString(1, c.getTitle());
+            ps.setString(2, c.getDescription());
+            ps.setInt(3, c.getUserId());
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
